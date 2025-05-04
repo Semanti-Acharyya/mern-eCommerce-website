f6fd9b890d1c9f7acdd52fbf8f88605fe9445fe1
Semanti-Acharyya <sacharyya2000@gmail.com>
2025-05-04 19:05:51 +0530

    Add Latest Collection section and products within it

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,10 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo} className="w-36" alt="" />
+      {/* Company logo */}
+      <Link to="/">
+        <img src={assets.logo} className="w-36" alt="" />
+      </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col itms-center gap-1">
