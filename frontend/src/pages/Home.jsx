3a1609739cb0fd91d20c32d74c163a74085fde65
Semanti-Acharyya <sacharyya2000@gmail.com>
2025-05-04 20:10:37 +0530

    Add Best Sellers section & products within it

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
+import BestSellers from "../components/BestSellers";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       {/* Mounting each components */}
       <Hero />
       <LatestCollection />
+      <BestSellers />
     </div>
   );
 };
